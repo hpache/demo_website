81734c13580ff0b78c408a01de673a56d710333e
hpache <shenryp78@gmail.com>
2021-11-13 13:08:13 -0500

    Fixing heroku

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from models import db, InfoModel
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://ghdcurxdtgbxqu:8f59105edbba75ccd115e3bae7ec41e336850cfd3f0e3af0fdd00e6cd45e5168@ec2-23-23-133-10.compute-1.amazonaws.com:5432/dok6t6itvuucf"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://ccnhrenkzxnrsz:3be3d7647008c9d280a91e44a8d27f72d744387a91ae7aba0debc434a9f8e0fe@ec2-54-166-167-192.compute-1.amazonaws.com:5432/d40cickjvcgvnf"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
